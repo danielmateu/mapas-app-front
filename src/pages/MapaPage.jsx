9656b584c8181891a88c464f0289098787ca159a
Daniel Mateu <danielmateu86@gmail.com>
2023-05-11 17:48:24 +0200

    Mantener referencia de latitud, longitud y zoom

--- src/pages/MapaPage.jsx
+++ src/pages/MapaPage.jsx
@@ -8,9 +8,9 @@ const mapBoxToken = import.meta.env.VITE_MAPBOX_TOKEN
 mapboxgl.accessToken = mapBoxToken;
 
 const puntoInicial = {
-    lng: 5,
-    lat: 34,
-    zoom: 2
+    lng: 2.1629,
+    lat: 41.3972,
+    zoom: 12.90
 }
 
 const MapaPage = () => {
@@ -18,6 +18,7 @@ const MapaPage = () => {
     const mapaDiv = useRef();
 
     const [mapa, setMapa] = useState(null)
+    const [coords, setCoords] = useState(puntoInicial)
 
     useEffect(() => {
         let map = new mapboxgl.Map({
@@ -31,8 +32,26 @@ const MapaPage = () => {
         setMapa(map)
     }, [])
 
+    useEffect(() => {
+        mapa?.on('move', () => {
+            const { lng, lat } = mapa.getCenter()
+            setCoords({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom: mapa.getZoom().toFixed(2)
+            })
+        })
+    }, [mapa])
+
+
     return (
         <>
+            <div
+                className="info fixed top-10 left-10 z-50 px-4 py-2 rounded-xl bg-white opacity-70 shadow-lg"
+            >
+                Lng: {coords.lng} | Lat: {coords.lat} | Zoom: {coords.zoom}
+            </div>
+
             <div
                 ref={mapaDiv}
                 className="mapContainer absolute bottom-0 left-0 right-0 top-0"
